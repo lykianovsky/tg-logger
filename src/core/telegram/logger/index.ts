e3519a6001f88612cae4b2a8ee6bb22bfded8e48
Ilya <ilyasorokoletov1@gmail.com>
2025-02-06 15:41:38 +0200

    feat: Add readme, and remove example code

--- src/core/telegram/logger/index.ts
+++ src/core/telegram/logger/index.ts
@@ -1,5 +1,4 @@
 import {TelegramClient, TelegramClientOptions} from '../client'
-import {TelegramLoggerMessageBuilder} from './message'
 
 interface TelegramLoggerOptions {
   clientOptions: TelegramClientOptions
@@ -18,51 +17,3 @@ export class TelegramLogger {
     void this.client.sendMessage(message)
   }
 }
-
-const cacheExpireTime = Date.now() + 300 * 1000
-const throttleInterval = 1000 * 10
-
-const logger = new TelegramLogger({
-  clientOptions: {
-    chatId: '-1002155204660',
-    token: '8055652628:AAH3yyKKiYYQ4yCGSdRnwj0xPj6ZcbUID3o',
-    cache: {
-      maxSize: 10_000,
-      ttl: cacheExpireTime,
-    },
-    throttle: {
-      interval: throttleInterval,
-      limit: 20,
-      maxSize: 10_000,
-    },
-    debug: true,
-  },
-})
-
-const error = new TelegramLoggerMessageBuilder()
-  .bold('❗ Server Errors')
-  .enter(2)
-  .underline('Message:')
-  .enter()
-  .code('Too many request status code 429')
-  .get()
-
-const info = new TelegramLoggerMessageBuilder()
-  .bold('🔵 Info')
-  .enter(2)
-  .underline('Message:')
-  .enter()
-  .code('Something information for colleagues')
-  .get()
-
-const warn = new TelegramLoggerMessageBuilder()
-  .bold('🟡 Warning')
-  .enter(2)
-  .underline('Message:')
-  .enter()
-  .code('We are a people!!!')
-  .get()
-
-logger.log(error)
-logger.log(info)
-logger.log(warn)
